43bffd33eff7abdd8ec7492c55073addae0f934e
Adam <adamfrew1999@gmail.com>
2021-03-27 03:04:19 +0000

    Message config and rate set working

--- Core/Inc/GPS.h
+++ Core/Inc/GPS.h
@@ -1,1 +1,17 @@
-int gps_msg_config(const char *nema, int rus1)
+/*
+ * GPS.h
+ *
+ *  Created on: Mar 26, 2021
+ *      Author: adamf
+ */
+#include "main.h"
+#include <stdint.h>
+
+#ifndef INC_GPS_H_
+#define INC_GPS_H_
+
+int gps_msg_config(UART_HandleTypeDef *huart, const char *nema, int rus1);
+int gps_rate_config(UART_HandleTypeDef *huart, uint16_t measRate, uint16_t navRate, uint16_t timeRef);
+int get_date_time(UART_HandleTypeDef *huart);
+
+#endif /* INC_GPS_H_ */
